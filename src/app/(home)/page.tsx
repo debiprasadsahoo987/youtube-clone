dcf84ebcb2f91d7a17e70e02c3a338ba621221e9
Debiprasad <debiprasadsahoo987@gmail.com>
2025-02-15 12:15:48 +0530

    modified basic home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Content</p>
+    </div>
+  );
 }
--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
-  return <Button>Sign In</Button>;
+  return (
+    <Button
+      variant="outline"
+      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+    >
+      <UserCircleIcon />
+      Sign In
+    </Button>
+  );
 };
